b38dbaeb613e64ed0cc58c6be2a0d113212a05dc
Samu <samu.uunonen@student.hamk.fi>
2021-10-08 09:51:13 +0300

    updates
    Added update blood type function.
    Small changes to SQLite db.js codes.

--- react-native/blood-tracker/components/BloodDropDownPicker.js
+++ react-native/blood-tracker/components/BloodDropDownPicker.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 
@@ -7,17 +7,25 @@ const BloodDropDownPicker = (props) => {
   const [value, setValue] = useState(null);
   const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    setValue(props.currentBloodType)
+  }, [])
+
   return (
     <DropDownPicker
       listMode="FLATLIST"
+      schema={{
+        label: "bloodType",
+        value: "bloodType"
+      }}
+      itemKey="id"
       open={open}
       setOpen={setOpen}
       items={items}
       setItems={setItems}
       value={value}
       setValue={setValue}
-      itemKey="value"
-      placeholder={props.items.length > 0 ? props.items[0].value : "Select"}
+      placeholder={props.currentBloodType === "" ? props.currentBloodType : "Select"}
       itemSeparator={true}
       closeAfterSelecting={true}
       onChangeValue={() => props.setSelectedBloodType(value)}
